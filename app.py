0090613602c9c0cbfe79fa464dc78246dcfebe70
Wassime Mezzache <mezzache.pro@gmail.com>
2024-03-21 14:59:34 +0100

    Modifications

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/get', methods=['GET'])
+def get_endpoint():
+    return 'GET endpoint called'
+
+@app.route('/post', methods=['POST'])
+def post_endpoint():
+    data = request.get_json()
+    return f"POST endpoint called with data: {data}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
