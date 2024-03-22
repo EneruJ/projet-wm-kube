943c5c6adbb2b59e94ae7989aeca606a86ba724b
Wassime Mezzache <mezzache.pro@gmail.com>
2024-03-22 14:31:47 +0100

    Clean

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255)
-);
-
-INSERT INTO users (name, email) VALUES ('John', 'john@example.com');
-INSERT INTO users (name, email) VALUES ('Emma', 'emma@example.com');
-INSERT INTO users (name, email) VALUES ('Michel', 'michael@example.com');
